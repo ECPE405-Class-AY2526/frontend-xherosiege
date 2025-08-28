5ee68c5102128bae4b84f8d86f508902e174a879
KyleLorenzCasianan <ckylelorenz@gmail.com>
2025-08-28 13:34:29 +0800

    removed dashboard button

--- client/src/Components/LandPageNav.jsx
+++ client/src/Components/LandPageNav.jsx
@@ -7,9 +7,6 @@ const LandPageNav = () => {
         <div className ="mx-auto max-w-10xl px-4 py-5 flex justify-between items-center mb-0">
             <div className="flex space-x-6">
                 <button className="text-black font-medium">Home</button>
-                <Link to='/dashboard"'>
-                    <button className="text-black font-medium">Dashboard</button>
-                </Link>
                 <button className='text-black font-medium'>About us</button>
                 <input type="text" placeholder="Search" className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
             </div>
